f774f04140daf8f6e239c2f895f54af4617cd517
ASCSagar <ascsagar08@gmail.com>
2025-02-24 17:59:56 +0530

    Vite-PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      manifest: {
+        name: "Sweekar",
+        short_name: "Sweekar",
+        description: "Sweekar",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/Sweekar.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/",
   resolve: {
     alias: {
